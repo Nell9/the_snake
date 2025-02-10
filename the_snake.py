db549b99462d4295beb3973513544fc63e50cbca
Рабочий пк <gmail.com>
2025-02-10 16:40:02 +0300

    Начало разработки

--- the_snake.py
+++ the_snake.py
@@ -8,22 +8,22 @@ GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 
+# Цвет фона - черный:
+BOARD_BACKGROUND_COLOR = (0, 0, 0)
+
 # Направления движения:
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-# Цвет фона - черный:
-BOARD_BACKGROUND_COLOR = (0, 0, 0)
+# Цвет границы ячейки - блекло-белый:
+BORDER_COLOR = (200, 200, 200)
 
-# Цвет границы ячейки
-BORDER_COLOR = (93, 216, 228)
-
-# Цвет яблока
+# Цвет яблока - красный:
 APPLE_COLOR = (255, 0, 0)
 
-# Цвет змейки
+# Цвет змейки - зеленый:
 SNAKE_COLOR = (0, 255, 0)
 
 # Скорость движения змейки:
@@ -33,57 +33,141 @@ SPEED = 20
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 
 # Заголовок окна игрового поля:
-pygame.display.set_caption('Змейка')
+pygame.display.set_caption("Змейка")
 
 # Настройка времени:
 clock = pygame.time.Clock()
 
 
-# Тут опишите все классы игры.
-...
+class GameObject:
+    """Описывает все игровые обьекты."""
+    position: tuple[int, int]
+    body_color: tuple[int, int, int]
 
+    def __init__(self):
+        self.position = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        
+    def draw(self):
+        pass
+
+
+class Apple(GameObject):
+    """Класс описывающий сущность <Яблоко>."""
+
+    def __init__(self):
+        super().__init__()
+        self.body_color = APPLE_COLOR
+        self.position = self.randomize_position()
+
+    def randomize_position(self) -> tuple[int, int]:
+        """Возвращает случайную позицию ячейки"""
+        return (randint(0, GRID_WIDTH), randint(0, GRID_HEIGHT))
+    
+    def draw(self):
+        rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
+        pygame.draw.rect(screen, self.body_color, rect)
+        
+    
+
+class Snake(GameObject):
+    """Класс описывающий сущность <Змейка>."""
+    _positions: list = []
+    __next_direction: tuple | None = None
+    __lenght: int = 1
+    _direction: tuple = RIGHT
+    
+    def __init__(self):
+        super().__init__()
+        self.body_color = SNAKE_COLOR
+
+    def update_direction(self):
+        if self.next_direction:
+            self.direction = self.next_direction
+            self.next_direction = None
+
+    def move(self):
+        ...
+
+    def draw(self):
+        for position in self.positions[:-1]:
+            rect = (pygame.Rect(position, (GRID_SIZE, GRID_SIZE)))
+            pygame.draw.rect(screen, self.body_color, rect)
+            pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
+
+        #Отрисовка головы змейки.
+        head_rect = pygame.Rect(self.positions[0], (GRID_SIZE, GRID_SIZE))
+        pygame.draw.rect(screen, self.body_color, head_rect)
+        pygame.draw.rect(screen, BORDER_COLOR, head_rect, 1)
+
+        #Затирание последнего сегмента.
+        if self.last:
+            last_rect = pygame.Rect(self.last, (GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
+    
+    @property
+    def get_head_position(self):
+        ...
+    
+    def reset(self):
+        ...
+
+
+def handle_keys(game_object):
+    """Обработчик событий."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and game_object.direction != DOWN:
+                game_object.next_direction = UP
+            elif event.key == pygame.K_DOWN and game_object.direction != UP:
+                game_object.next_direction = DOWN
+            elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
+                game_object.next_direction = LEFT
+            elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
+                game_object.next_direction = RIGHT
 
 def main():
     # Инициализация PyGame:
     pygame.init()
-    # Тут нужно создать экземпляры классов.
-    ...
+    # Objects:
+    apple = Apple()
+    snake = Snake()
 
-    # while True:
-    #     clock.tick(SPEED)
+    running = True
+    
+    while running:
+        handle_keys(snake)
+        
+        apple.draw()
+        pygame.display.flip()
+        clock.tick(SPEED)
 
-        # Тут опишите основную логику игры.
-        # ...
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
 
-# Метод draw класса Apple
-# def draw(self):
-#     rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
-#     pygame.draw.rect(screen, self.body_color, rect)
-#     pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
 
-# # Метод draw класса Snake
+
+# Метод draw класса Snake.
 # def draw(self):
 #     for position in self.positions[:-1]:
 #         rect = (pygame.Rect(position, (GRID_SIZE, GRID_SIZE)))
 #         pygame.draw.rect(screen, self.body_color, rect)
 #         pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
 
-#     # Отрисовка головы змейки
+#     Отрисовка головы змейки.
 #     head_rect = pygame.Rect(self.positions[0], (GRID_SIZE, GRID_SIZE))
 #     pygame.draw.rect(screen, self.body_color, head_rect)
 #     pygame.draw.rect(screen, BORDER_COLOR, head_rect, 1)
 
-#     # Затирание последнего сегмента
+#     Затирание последнего сегмента.
 #     if self.last:
 #         last_rect = pygame.Rect(self.last, (GRID_SIZE, GRID_SIZE))
 #         pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
 
-# Функция обработки действий пользователя
+# Функция обработки действий пользователя.
 # def handle_keys(game_object):
 #     for event in pygame.event.get():
 #         if event.type == pygame.QUIT:
@@ -99,7 +183,7 @@ if __name__ == '__main__':
 #             elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
 #                 game_object.next_direction = RIGHT
 
-# Метод обновления направления после нажатия на кнопку
+# Метод обновления направления после нажатия на кнопку.
 # def update_direction(self):
 #     if self.next_direction:
 #         self.direction = self.next_direction
